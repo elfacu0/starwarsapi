df7cf827a31c072cc6321ec2647e90a6d23db27e
k- <mahat.mejor@gmail.com>
2020-01-25 22:27:01 -0600

    Updates

---
+++ precache-manifest.bf13bb014864b74db8ad824d05d8cbf0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d411dd91eef3fc832bc52eeff3e8369",
+    "url": "/starwarsapi/index.html"
+  },
+  {
+    "revision": "9eeb1901308de08b6cf4",
+    "url": "/starwarsapi/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1c656f0b0c9647227ab0",
+    "url": "/starwarsapi/static/css/main.2e7f5aa8.chunk.css"
+  },
+  {
+    "revision": "9eeb1901308de08b6cf4",
+    "url": "/starwarsapi/static/js/2.343adb4c.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/starwarsapi/static/js/2.343adb4c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1c656f0b0c9647227ab0",
+    "url": "/starwarsapi/static/js/main.eb5de0e1.chunk.js"
+  },
+  {
+    "revision": "7f0d2459731cc5b50f08",
+    "url": "/starwarsapi/static/js/runtime-main.efe3256d.js"
+  }
+]);
